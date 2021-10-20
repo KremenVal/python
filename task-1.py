b5e8d9c9f553773a53b4743d877e1830688046a1
kremen <kremen.val@gmail.com>
2021-10-20 08:24:36 +0300

    (new task) - added file with home task number 1

---
+++ task-1.py
@@ -0,0 +1,13 @@
+num_int = 1
+num_float = 1.1
+text_str = 'Hello World'
+is_true = True
+num_user_1 = int(input('Please, enter the number - '))
+num_user_2 = int(input('Please, enter the number - '))
+text_1 = input('Please, enter any text - ')
+text_2 = input('Please, enter any text - ')
+
+print(f"{num_int} - type is {type(num_int)}\n{num_float} - type is {type(num_float)}\n{text_str} - type is"
+      f" {type(text_str)}\n{is_true} - type is {type(is_true)}\n")
+print(f"{num_user_1} - type is {type(num_user_1)}\n{num_user_2} - type is {type(num_user_2)}\n{text_1} - type is "
+      f"{type(text_1)}\n{text_2} - type is {type(text_2)}")
