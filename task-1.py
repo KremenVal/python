70b33987d99289802ebb05f047e57100daf1e29a
kremen <kremen.val@gmail.com>
2021-10-22 09:18:03 +0300

    (added home task) - added home task 1 for lesson 2

--- task-1.py
+++ task-1.py
@@ -1,13 +1,7 @@
-num_int = 1
-num_float = 1.1
-text_str = 'Hello World'
-is_true = True
-num_user_1 = int(input('Введите число - '))
-num_user_2 = int(input('Введите число - '))
-text_1 = input('Введите любой текст - ')
-text_2 = input('Введите любой текст - ')
+my_list = [
+    1, 'string', [1, 'string', tuple('Hello World!'), set('Hello World!'), {'1': 1, '2': 2}, True, bytes(27), None],
+    tuple('Hello World!'), set('Hello World!'), {'1': 1, '2': 2}, True, bytes(27), None
+]
 
-print(f"{num_int} - {type(num_int)}\n{num_float} - {type(num_float)}\n{text_str} - {type(text_str)}\n{is_true} - "
-      f"{type(is_true)}\n")
-print(f"{num_user_1} - {type(num_user_1)}\n{num_user_2} - {type(num_user_2)}\n{text_1} - {type(text_1)}\n{text_2} - "
-      f"{type(text_2)}")
+for i in my_list:
+    print(type(i))
