ee77edb60cdb8df242c74013c974990ff753cebd
kremen <kremen.val@gmail.com>
2021-10-28 07:05:09 +0300

    (added home task) - added home task 1 and some checking functions for lesson 3

--- task-1.py
+++ task-1.py
@@ -1,7 +1,18 @@
-my_list = [
-    1, 'string', [1, 'string', tuple('Hello World!'), set('Hello World!'), {'1': 1, '2': 2}, True, bytes(27), None],
-    tuple('Hello World!'), set('Hello World!'), {'1': 1, '2': 2}, True, bytes(27), None
-]
+from checks import check_for_num, check_for_int
 
-for i in my_list:
-    print(type(i))
+
+def my_div(a, b):
+    return a / b if b != 0 else print('Деление на 0!')
+
+
+nums = {1: 0, 2: 0}
+
+for key, value in nums.items():
+    while True:
+        num = input(f'Введите число {key} - ')
+
+        if check_for_num(num):
+            nums[key] = int(num) if check_for_int(num) else float(num)
+            break
+
+print(my_div(nums[1], nums[2]))
