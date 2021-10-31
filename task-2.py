0fbb33555ed61c5ac5a0b31ad51e7e4f9be67787
kremen.val@gmail.com <kremen.val@gmail.com>
2021-10-31 18:55:29 +0100

    (added home task) - added home task 2 for lesson 4

--- task-2.py
+++ task-2.py
@@ -1,10 +1,4 @@
-user_time = int(input('Введите любое число в секундах (например, 360) - '))
+import random
 
-if user_time < 0:
-    print('Время не может быть отрецательным.')
-else:
-    hours = str(user_time // 3600)
-    minutes = str((user_time - int(hours) * 3600) // 60)
-    seconds = str(user_time - (int(hours) * 3600) - (int(minutes) * 60))
-    print("{}:{}:{}".format(hours if len(hours) > 1 else '0' + hours, minutes if len(minutes) > 1 else '0' + minutes,
-                            seconds if len(seconds) > 1 else '0' + seconds))
+my_list = [random.randrange(0, 1000) for _ in range(0, 10)]
+print([my_list[i + 1] for i in range(0, len(my_list) - 1) if my_list[i + 1] > my_list[i]])
