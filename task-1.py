eb42c0bcdd207d052d80e3b1f0a971d7c14331bf
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-01 16:13:27 +0100

    (added home task) - added home task 1 for lesson 4

--- task-1.py
+++ task-1.py
@@ -1,5 +1,10 @@
 from checks import is_num, is_int
 
+
+def salary(output_in_hours=10, rate_per_hour=100, bonus=0):
+	return (output_in_hours * rate_per_hour) + bonus
+
+
 my_dict = {'выработку': 0, 'ставку': 0, 'премия': 0}
 
 for key, value in my_dict.items():
@@ -9,3 +14,7 @@ for key, value in my_dict.items():
 		if is_num(num):
 			my_dict[key] = int(num) if is_int(num) else float(num)
 			break
+		elif key == 'премия' and num == '':
+			break
+
+print(salary(my_dict['выработку'], my_dict['ставку'], my_dict['премия']))
