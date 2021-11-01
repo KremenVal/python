eaba234433e8a78ce96653b411c5eb4887a5e13f
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-01 08:03:44 +0100

    (added home task) - added home task 1 and file with checks for lesson 4

--- task-1.py
+++ task-1.py
@@ -1,13 +1,11 @@
-num_int = 1
-num_float = 1.1
-text_str = 'Hello World'
-is_true = True
-num_user_1 = int(input('Введите число - '))
-num_user_2 = int(input('Введите число - '))
-text_1 = input('Введите любой текст - ')
-text_2 = input('Введите любой текст - ')
+from checks import is_num, is_int
 
-print(f"{num_int} - {type(num_int)}\n{num_float} - {type(num_float)}\n{text_str} - {type(text_str)}\n{is_true} - "
-      f"{type(is_true)}\n")
-print(f"{num_user_1} - {type(num_user_1)}\n{num_user_2} - {type(num_user_2)}\n{text_1} - {type(text_1)}\n{text_2} - "
-      f"{type(text_2)}")
+my_dict = {'выработку': 0, 'ставку': 0, 'премия': 0}
+
+for key, value in my_dict.items():
+	while True:
+		num = input(f'Введите {key} - ')
+
+		if is_num(num):
+			my_dict[key] = int(num) if is_int(num) else float(num)
+			break
