511cab48ce0793fd4f23f8b31a1923f81f1a7cdf
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-05 19:08:24 +0100

    (added home task) - added home task 3 and for lesson 5

--- task-3.py
+++ task-3.py
@@ -1,1 +1,16 @@
-print([i for i in range(20, 240) if i % 20 == 0 or i % 21 == 0])
+employees, sum_of_salary = {}, 0
+
+with open('task-3.txt', 'r', encoding='utf-8') as file:
+	for line in file:
+		key, value = line.split()
+
+		if key in employees:
+			employees[key].append(float(value))
+		else:
+			employees[key] = [float(value)]
+
+for employee, salaries in employees.items():
+	[print(f"Сотрудник {employee} имеет оклад {salary}.") for salary in salaries if salary < 20000]
+	sum_of_salary += sum(salaries)
+
+print(f"Средняя величина дохода сотрудников - {sum_of_salary / len(employees)}")
