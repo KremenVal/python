c8fa91c2d38a342ec546270b5e1cfc57c91caf10
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-06 21:53:58 +0100

    (added home task) - added home task 4 and for lesson 6

--- task-4.py
+++ task-4.py
@@ -1,5 +1,90 @@
-nums = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре'}
+class Car:
+	speed = 0
+	color = ''
+	name = ''
+	is_police = True
 
-with open('task-4-1.txt', 'r', encoding='utf-8') as file_1:
-	with open('task-4-2.txt', 'w', encoding='utf-8') as file_2:
-		[file_2.write(line.replace(line.split()[0], nums[line.split()[0].lower()])) for line in file_1]
+	def __init__(self, speed, color, name, is_police):
+		self.speed = speed
+		self.color = color
+		self.name = name
+		self.is_police = is_police
+
+	def go(self):
+		return 'Машина поехала'
+
+	def stop(self):
+		return 'Машина остановилась'
+
+	def turn(self, direction):
+		return f"Машина повернула на {direction}"
+
+	def show_speed(self):
+		return self.speed
+
+
+class TownCar(Car):
+	def show_speed(self):
+		if self.speed > 60:
+			return 'Превышение скорости'
+		else:
+			return Car.show_speed(self)
+
+
+class SportCar(Car):
+	pass
+
+
+class WorkCar(Car):
+	def show_speed(self):
+		if self.speed > 40:
+			return 'Превышение скорости'
+		else:
+			return Car.show_speed(self)
+
+
+class PoliceCar(Car):
+	pass
+
+
+townCar_1 = TownCar(65, 'red', 'toyota', False)
+townCar_2 = TownCar(55, 'red', 'toyota', False)
+sportCar = SportCar(300, 'yellow', 'lamborgini', False)
+workCar_1 = WorkCar(50, 'black', 'nissan', False)
+workCar_2 = WorkCar(35, 'black', 'nissan', False)
+policeCar = PoliceCar(70, 'white', 'mercedes', True)
+print(townCar_1.speed)
+print(townCar_1.color)
+print(townCar_1.name)
+print(townCar_1.is_police)
+print(townCar_1.show_speed())
+print()
+print(townCar_2.speed)
+print(townCar_2.color)
+print(townCar_2.name)
+print(townCar_2.is_police)
+print(townCar_2.show_speed())
+print()
+print(sportCar.speed)
+print(sportCar.color)
+print(sportCar.name)
+print(sportCar.is_police)
+print(sportCar.show_speed())
+print()
+print(workCar_1.speed)
+print(workCar_1.color)
+print(workCar_1.name)
+print(workCar_1.is_police)
+print(workCar_1.show_speed())
+print()
+print(workCar_2.speed)
+print(workCar_2.color)
+print(workCar_2.name)
+print(workCar_2.is_police)
+print(workCar_2.show_speed())
+print()
+print(policeCar.speed)
+print(policeCar.color)
+print(policeCar.name)
+print(policeCar.is_police)
+print(policeCar.show_speed())
