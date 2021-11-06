714e3c0286d9bc990e28885e258cdb92f3065560
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-06 17:12:06 +0100

    (added home task) - added home task 4 and for lesson 5

--- task-4.py
+++ task-4.py
@@ -1,4 +1,5 @@
-from random import randrange
+nums = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре'}
 
-my_list = [randrange(0, 1000) for _ in range(0, 10)]
-print([i for i in my_list if my_list.count(i) == 1])
+with open('task-4-1.txt', 'r', encoding='utf-8') as file_1:
+	with open('task-4-2.txt', 'w', encoding='utf-8') as file_2:
+		[file_2.write(line.replace(line.split()[0], nums[line.split()[0].lower()])) for line in file_1]
