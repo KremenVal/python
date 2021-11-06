bc04d8880f909a2996cb98910433910db43f29bb
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-06 18:13:50 +0100

    (added home task) - added home task 6 and for lesson 5

--- task-6.py
+++ task-6.py
@@ -1,20 +1,6 @@
-from itertools import count, cycle
-from random import randrange
+with open('task-6.txt', 'r', encoding='utf-8') as file:
+    my_dict = {f"{line.split(':')[0]}": line.split(':')[1].replace('—', '').replace('-', '') for line in file}
+    result = {f"{key}": sum(map(lambda el: int(el), [item[0:item.find('(')] for item in value.strip().split()]))
+              for key, value in my_dict.items()}
 
-
-num_start = randrange(0, 100)
-num_end = randrange(100, 150)
-
-for i in count(num_start):
-	print(i)
-
-	if num_end == i:
-		break
-
-my_list = [randrange(0, 100) for _ in range(0, 10)]
-iter_list = cycle(my_list)
-end_cycle = randrange(10, 50)
-
-while end_cycle > 0:
-	print(next(iter_list))
-	end_cycle -= 1
+print(result)
