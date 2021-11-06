be0376b80f0a9b7cfe0ef3097351c64013ae511f
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-06 20:28:51 +0100

    (added home task) - added home task 2 and for lesson 6

--- task-2.py
+++ task-2.py
@@ -1,2 +1,16 @@
-with open('task-2.txt', 'r', encoding='utf8') as file:
-	[print(f"{num + 1}. {line.strip()}\nКоличество слов в строке - {len(line.split())}.") for num, line in enumerate(file)]
+class Road:
+	_length = 0
+	_width = 0
+	mass_asphalt = 25
+	blade_thickness = 5
+
+	def __init__(self, length, width):
+		self.length = length
+		self.width = width
+
+	def mass_of_asphalt(self):
+		return self.length * self.width * self.mass_asphalt * self.blade_thickness / 1000
+
+
+new_road = Road(20, 5000)
+print(new_road.mass_of_asphalt())
