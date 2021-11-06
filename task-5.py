7395cb20129b28e491a19866f5412cb5d499eca6
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-06 17:41:43 +0100

    (added home task) - added home task 5 and for lesson 5

--- task-5.py
+++ task-5.py
@@ -1,4 +1,7 @@
-from functools import reduce
+from random import randrange
 
 
-print(reduce(lambda a, b: a * b, [i for i in range(100, 1001) if i % 2 == 0]))
+with open('task-5.txt', 'w+') as file:
+	file.write(' '.join([str(randrange(0, 1000)) for _ in range(0, 1000)]))
+	file.seek(0)
+	print(sum(map(lambda element: int(element), file.readline().split())))
