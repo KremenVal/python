f48d9e052022f0fd923bc9a786d6f9c82c653f8e
kremen.val@gmail.com <kremen.val@gmail.com>
2021-11-15 19:51:25 +0100

    (added home task) - added home task 4 and for lesson 8

--- task-4.py
+++ task-4.py
@@ -3,37 +3,31 @@ class Warehouse:
 
 
 class Equipment:
-	quantity = 0
 	cost = 0
 
 
 class Printer(Equipment):
-	def __init__(self, quantity, cost, name, color, weight):
-		self.quantity = quantity
+	def __init__(self, cost, name, weight):
 		self.cost = cost
 		self.name = name
-		self.color = color
 		self.weight = weight
 
 
 class Scanner(Equipment):
-	def __init__(self, quantity, cost, name, color, height):
-		self.quantity = quantity
+	def __init__(self, cost, name, height):
 		self.cost = cost
 		self.name = name
-		self.color = color
 		self.height = height
 
 
 class Xerox(Equipment):
-	def __init__(self, quantity, cost, name, color, width):
-		self.quantity = quantity
+	def __init__(self, cost, name, width):
 		self.cost = cost
 		self.name = name
-		self.color = color
 		self.width = width
 
 
-# printer = Printer(1, 2, 'Printer', 'white', 1000)
-# scanner = Scanner(1, 3, 'Scanner', 'black', 30)
-# xerox = Xerox(1, 1, 'Xerox', 'red', 40)
+# printer = Printer(2000, 'Printer', 1000)
+# scanner = Scanner(3000, 'Scanner', 30)
+# xerox = Xerox(1000, 'Xerox', 40)
+# print(printer, scanner, xerox)
